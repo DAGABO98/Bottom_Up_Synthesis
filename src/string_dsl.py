178020818bd11040ed44efcb10daa943533f753d
Daniel Garces <dg3008@columbia.edu>
2023-10-18 13:07:01 -0400

    Fix constant extraction for strings DSL

--- src/string_dsl.py
+++ src/string_dsl.py
@@ -1,3 +1,5 @@
+import difflib
+
 class String_dsl:
 
     def __init__(self):
@@ -127,12 +129,41 @@ class String_dsl:
                 return self.execute_op(parse_tree[0], evaluated_args)
         else:
             return parse_tree
+    
+    def _get_string_overlap(self, example1, example2):
+        seq_match = difflib.SequenceMatcher(None, example1, example2)
+        pos_a, _, size = seq_match.find_longest_match(0, len(example1), 0, len(example2))
+        return example1[pos_a: pos_a+size]
+    
+    def _get_max_output_overlap(self, output_examples):
+        max_output_match = ""
+        for output_index in range(len(output_examples)-1):
+            if output_index == 0:
+                max_output_match = self._get_string_overlap(output_examples[output_index], output_examples[output_index+1])
+            else:
+                max_output_match = self._get_string_overlap(max_output_match, output_examples[output_index+1])
         
-    def _generate_string_sets(self, input_string, output_string):
-        input_string_set = set(input_string)
-        output_string_set = set(output_string)
-        constant_set = input_string_set.union(output_string_set.difference(input_string_set))
-        return constant_set
+        return max_output_match
+    
+    def _get_max_input_overlap(self, input_examples):
+        max_input_match = ""
+        for input_index in range(len(input_examples)-1):
+            if len(input_examples[input_index]) == 1:
+                if input_index == 0:
+                    max_input_match = self._get_string_overlap(input_examples[input_index][0], input_examples[input_index+1][0])
+                else:
+                    max_input_match = self._get_string_overlap(max_input_match, input_examples[input_index+1][0])
+            else:
+                for i in range(len(input_examples[input_index])-1):
+                    if input_index == 0 and input_index == 0:
+                        max_input_match = self._get_string_overlap(input_examples[input_index][i], input_examples[input_index][i+1])
+                    else:
+                        max_input_match = self._get_string_overlap(max_input_match, input_examples[input_index][i+1])
+                
+                for i in range(len(input_examples[input_index+1])):
+                    max_input_match = self._get_string_overlap(max_input_match, input_examples[input_index+1][i])
+        
+        return max_input_match
     
     def extract_constants(self, input_examples, output_examples):
         int_constants = [0, 1, 2]
@@ -141,25 +172,27 @@ class String_dsl:
             constants_for_examples = [constant_value for _ in range(len(input_examples))]
             int_constant_list.append(("int", (constant_value, constants_for_examples)))
 
-        input_constant_set = set()
-        for input_index in range(len(input_examples)):
-            input_example = input_examples[input_index]
-            output_example = output_examples[input_index]
-            if len(input_example) == 1:
-                input_string = input_example[0]
-                temp_input_constant_set = self._generate_string_sets(input_string=input_string, 
-                                                                     output_string=output_example)
-                input_constant_set |= temp_input_constant_set
+        max_output_match = self._get_max_output_overlap(output_examples)
+        max_output_match_set = set(max_output_match)
 
-            else:
-                for i in range(len(input_example)):
-                    input_string = input_example[i]
-                    temp_input_constant_set = self._generate_string_sets(input_string=input_string, 
-                                                                         output_string=output_example)
-                    input_constant_set |= temp_input_constant_set
+        max_input_match = self._get_max_input_overlap(input_examples)
+        max_input_match_set = set(max_input_match)
+
+        default_str_constants = [" ", ",", ":"]
+
+        str_constant_set = max_output_match_set | max_input_match_set
+
+        IO_str_constants = []
+        for str_constant in str_constant_set:
+            IO_str_constants.append(str_constant)
+        
+        IO_str_constants.append(max_input_match)
+        IO_str_constants.append(max_output_match)
+
+        str_constants = default_str_constants + IO_str_constants
         
         str_constant_list = []
-        for string_constant in input_constant_set:
+        for string_constant in str_constants:
             constants_for_examples = [string_constant for _ in range(len(input_examples))]
             str_constant_list.append(("str", (string_constant, constants_for_examples)))
 
